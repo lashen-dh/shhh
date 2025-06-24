27d17f66e9bd7b5039fd1e4ed56849930dfb90ed
dharml@kardinia.vic.edu.au <dharml@kardinia.vic.edu.au>
2025-06-24 08:16:52 +0000

    Edited Quiz

--- client_code/Quiz/__init__.py
+++ client_code/Quiz/__init__.py
@@ -8,20 +8,15 @@ import random
 #TODO: Make it so that the questions chosen match student ELO
 class Question():
   def __init__(self, strType, strInstruction, strDetails, strAnswers, strCorrectAnswer, intElo):
-    self.dictDetails = { 'intElo': intElo, 'strType': strType, 'strInstruction': strInstruction, 'strDetails': strDetails, 'strAnswers': strAnswers, 'strInstruction': strInstruction, 'strCorrectAnswer': strCorrectAnswer, 'arrAnswers': strAnswers.split(",") }
+    self.dictDetails = { 'intElo': intElo, 'strType': strType, 'strInstruction': strInstruction, 'strDetails': strDetails, 'strAnswers': strAnswers, 'strCorrectAnswer': strCorrectAnswer, 'arrAnswers': strAnswers.split(",") }
     
 class Quiz(QuizTemplate):
   def assignToButtons(self, question):
     #Display random order for button questions
-    arrAnswerButtons, arrAlreadyChosenAnswers = [[self.btnAns1, self.btnAns2, self.btnAns3, self.btnAns4], []]
-    strChosenAnswer = question.dictDetails['arrAnswers'][random.randint(0, 3)]
-    
-    for btnAnswer in arrAnswerButtons:
-      #Keep choosing a random answer until we find one we haven't already put on a button
-      while strChosenAnswer in arrAlreadyChosenAnswers:
-        strChosenAnswer = question.dictDetails['arrAnswers'][random.randint(0, 3)]
-      arrAlreadyChosenAnswers.append(strChosenAnswer)
-      btnAnswer.text = strChosenAnswer
+    arrAnswerButtons = [self.btnAns1, self.btnAns2, self.btnAns3, self.btnAns4]
+    random.shuffle(question.dictDetails['arrAnswers'])
+    for i, btn in enumerate(arrAnswerButtons):
+      btn.text = question.dictDetails['arrAnswers'][i]
     
   def displayQuestion(self, num):
     self.lblQuestionNumber.text = f"Question Number {self.quizData['intQuestionNumber'] + 1}/{self.quizData['intMaxNumberOfQuestions']}"
@@ -38,17 +33,18 @@ class Quiz(QuizTemplate):
     return self.displayQuestion(self.quizData['intQuestionNumber'])
 
   def setupQuestionQueue(self):
-    #Choose a random index for random question
-    arrQuestionQueue = []
-    intChosenQuestionIndex = random.randint(0, self.quizData['intMaxNumberOfQuestions'] - 1)
-    for intQuestionIndex in range(self.quizData['intMaxNumberOfQuestions']):
-      while intChosenQuestionIndex in [key[0] for key in arrQuestionQueue]: #If the question selected already has been displayed, we keep looping until we get an index that hasn't been chosen before 
-        intChosenQuestionIndex = random.randint(0, self.quizData['intMaxNumberOfQuestions'] - 1) #We subtract one from intMaxNumberOfQuestions since the array indexes start at zero, while the intMaxNumberOfQuestions starts at 1
-      #Now that we've chosen an index that hasn't been chosen before, store it into the already chosen array to prvent it being selected again later
-      chosenQuestionDatabaseReference = self.quizData['tblQuestions'][intChosenQuestionIndex]  
-      #Add a question to the question queue with a new Question object created using database data for that respective question
-      arrQuestionQueue.append((intChosenQuestionIndex, Question(chosenQuestionDatabaseReference['type'], chosenQuestionDatabaseReference['instruction'], chosenQuestionDatabaseReference['details'], chosenQuestionDatabaseReference['answers'], chosenQuestionDatabaseReference['correctAnswer'], chosenQuestionDatabaseReference['elo'])))
-    return arrQuestionQueue
+    # Create a list of tuples where each tuple contains (index, Question object)
+    arrQuestions = [
+      (i, Question(
+        q['type'], q['instruction'], q['details'], q['answers'], q['correctAnswer'], q['elo']
+      )) for i, q in enumerate(self.quizData['tblQuestions'])
+    ]
+
+    # Sort the list of questions by the absolute difference between the student's Elo and the question's Elo
+    arrQuestions.sort(key=lambda x: abs(x[1].dictDetails['intElo'] - self.quizData['intStudentElo']))
+
+    # Return the sorted list
+    return arrQuestions
     
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -72,6 +68,7 @@ class Quiz(QuizTemplate):
     self.lblTimeTaken.text = f"Time Taken: {self.intTime['minutes']}:{self.intTime['seconds']:02d}"
 
   def eloChange(self, correct, **event_args):
+    print(correct)
     #ELO calculation
     k = 32
     floatExpectedStudentScore = 1 / (1 + 10**((self.quizData['currentQuestion'].dictDetails['intElo'] - self.quizData['intStudentElo']) / 400))
@@ -80,17 +77,11 @@ class Quiz(QuizTemplate):
   
     self.quizData['intStudentElo'] += floatEloDelta
     self.quizData['currentQuestion'].dictDetails['intElo'] -= floatEloDelta
-
     self.lblScore.text = self.quizData['intStudentElo']
+    
     #Remove question from database, first searching it up and gaining reference to it within the table
     currentQuestionInDatabase = app_tables.tblquestions.search(type=self.quizData['currentQuestion'].dictDetails['strType'], instruction=self.quizData['currentQuestion'].dictDetails['strInstruction'], details=self.quizData['currentQuestion'].dictDetails['strDetails'])
-
-    #Delete every column in the question
-    for column in currentQuestionInDatabase: 
-      column.delete()
-
-    #Add question back with updated values
-    app_tables.tblquestions.add_row(type=self.quizData['currentQuestion'].dictDetails['strType'], instruction=self.quizData['currentQuestion'].dictDetails['strInstruction'], details=self.quizData['currentQuestion'].dictDetails['strDetails'], answers=self.quizData['currentQuestion'].dictDetails['strAnswers'], correctAnswer=self.quizData['currentQuestion'].dictDetails['strCorrectAnswer'], elo=self.quizData['currentQuestion'].dictDetails['intElo'])
+    currentQuestionInDatabase[0].update(elo=self.quizData['currentQuestion'].dictDetails['intElo'])
     
   def btnSubmit_click(self, **event_args):
     #Multiple Choice Question
@@ -105,18 +96,20 @@ class Quiz(QuizTemplate):
       else:
         open_form('ResultsPage')
 
+  def toggleAnswerButton(self, index, **event_args):
+    self.quizData['intSelectedButton'] = index
+    buttons = [self.btnAns1, self.btnAns2, self.btnAns3, self.btnAns4]
+    for i, btn in enumerate(buttons):
+      btn.appearance = "filled" if i == index else "outlined"
+
   def btnAns1_click(self, **event_args):
-    self.quizData['intSelectedButton'] = 0
-    self.btnAns1.appearance, self.btnAns2.appearance, self.btnAns3.appearance, self.btnAns4.appearance = ["filled", "outlined", "outlined", "outlined"]
+    self.toggleAnswerButton(0)
 
   def btnAns2_click(self, **event_args):
-    self.quizData['intSelectedButton'] = 1
-    self.btnAns1.appearance, self.btnAns2.appearance, self.btnAns3.appearance, self.btnAns4.appearance = ["outlined", "filled", "outlined", "outlined"]
-    
+    self.toggleAnswerButton(1)
+
   def btnAns3_click(self, **event_args):
-    self.quizData['intSelectedButton'] = 2
-    self.btnAns1.appearance, self.btnAns2.appearance, self.btnAns3.appearance, self.btnAns4.appearance = ["outlined", "outlined", "filled", "outlined"]
+    self.toggleAnswerButton(2)
 
   def btnAns4_click(self, **event_args):
-    self.quizData['intSelectedButton'] = 3
-    self.btnAns1.appearance, self.btnAns2.appearance, self.btnAns3.appearance, self.btnAns4.appearance = ["outlined", "outlined", "outlined", "filled"]
+    self.toggleAnswerButton(3)
